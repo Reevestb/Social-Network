31fd48f8cd1fce8de3203bbcf3e3fcd6e5b4795e
Theo Reeves <theo_25@icloud.com>
2024-09-06 02:02:52 +0100

    hidden hover card for tablet mobile devices

--- src/components/HoverCard.jsx
+++ src/components/HoverCard.jsx
@@ -1,28 +1,62 @@
+// import React from "react";
+// import * as HoverCard from "@radix-ui/react-hover-card";
+// import { FaInfo } from "react-icons/fa";
+
+// //! My Primitive component from radix-ui
+
+// const HoverCardDemo = () => (
+//   <HoverCard.Root>
+//     <HoverCard.Trigger asChild>
+//       <a>
+//         <p className="bg-green-700  text-white  rounded-lg p-1 text-xl">
+//           {/* Helpful Hover Info */}
+//           <FaInfo />
+//         </p>
+//       </a>
+//     </HoverCard.Trigger>
+//     <HoverCard.Portal>
+//       <HoverCard.Content className="HoverCard" side={"bottom"}>
+//         <div>
+//           <div className="Text text-green-700">
+//             Useful Tips: Please use the `Edit Profile Button` to update your
+//             profile. To change your Avatar please use the `Manage Account`.
+//           </div>
+//         </div>
+
+//         <HoverCard.Arrow className="HoverCardArrow" />
+//       </HoverCard.Content>
+//     </HoverCard.Portal>
+//   </HoverCard.Root>
+// );
+
+// export default HoverCardDemo;
+
+// components/HoverCardDemo.js
+
 import React from "react";
 import * as HoverCard from "@radix-ui/react-hover-card";
 import { FaInfo } from "react-icons/fa";
-
-//! My Primitive component from radix-ui
 
 const HoverCardDemo = () => (
   <HoverCard.Root>
     <HoverCard.Trigger asChild>
       <a>
-        <p className="bg-green-700  text-white rounded-lg p-1 text-xl">
-          {/* Helpful Hover Info */}
+        <p
+          id="HoverCard"
+          className="bg-green-700 text-white rounded-lg p-1 text-xl"
+        >
           <FaInfo />
         </p>
       </a>
     </HoverCard.Trigger>
     <HoverCard.Portal>
-      <HoverCard.Content className="HoverCard" side={"bottom"}>
+      <HoverCard.Content id="HoverCard" side={"bottom"}>
         <div>
-          <div className="Text text-black">
+          <div className="Text text-green-700">
             Useful Tips: Please use the `Edit Profile Button` to update your
             profile. To change your Avatar please use the `Manage Account`.
           </div>
         </div>
-
         <HoverCard.Arrow className="HoverCardArrow" />
       </HoverCard.Content>
     </HoverCard.Portal>
