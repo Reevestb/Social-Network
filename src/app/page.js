3dc829c1afb568383c4f7ce1b8fea16963f5195d
Theo Reeves <theo_25@icloud.com>
2024-09-06 02:23:12 +0100

    added name to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+import Image from "next/image";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <h1>Welcome to the Social Network</h1>
+    <main className="flex min-h-screen flex-col items-center">
+      <h1 className="text-[2em] font-semibold text-green-700">Green Grass</h1>
+      <Image src="/images/gg.png" alt="Green Grass" height={500} width={500} />
+      <p className="text-[14px] text-green-400">
+        The Social Network where the grass is always greener
+      </p>
     </main>
   );
 }
